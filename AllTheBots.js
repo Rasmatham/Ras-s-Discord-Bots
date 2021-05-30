d12cf4b5099d8b0ea47ad978b64d6ead310c686a
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2021-05-30 18:49:25 +0200

    Readied Dice function to be modulified

--- AllTheBots.js
+++ AllTheBots.js
@@ -106,85 +106,83 @@
 		}
 		//dice
 		{
-			var dice = (bot) => {
-				bot.on(`message`, (message) => {
-					if (message.content.toLowerCase() !== `d`) {
-						if (message.content.toLowerCase().startsWith(`d`)) {
-							if (isNaN(Number(message.content.toLowerCase().replace(`d`, ``)))) {
-								if (message.content.includes(`*`)) {
-									var multidice = message.content
-										.toLowerCase()
-										.replace(`d`, ``)
-										.replace(`*`, ` `)
-										.split(` `);
-									var i;
-									if (isNaN(multidice[0])) {
-										return;
-									} else if (isNaN(multidice[1])) {
-										return;
-									} else if (multidice[1] >= 21) {
-										message.channel.send(`Sorry, but you can only do 20 at a time`);
+			var dice = (message, prefix) => {
+				if (message.content.toLowerCase() !== `${prefix}d`) {
+					if (message.content.toLowerCase().startsWith(`${prefix}d`)) {
+						if (isNaN(Number(message.content.toLowerCase().replace(`${prefix}d`, ``)))) {
+							if (message.content.includes(`*`)) {
+								var multidice = message.content
+									.toLowerCase()
+									.replace(`${prefix}d`, ``)
+									.replace(`*`, ` `)
+									.split(` `);
+								var i;
+								if (isNaN(multidice[0])) {
+									return;
+								} else if (isNaN(multidice[1])) {
+									return;
+								} else if (multidice[1] >= 21) {
+									message.channel.send(`Sorry, but you can only do 20 at a time`);
+								} else {
+									if (multidice[0] === `10`) {
+										for (i = 0; i < multidice[1]; i++) {
+											var number10 = new Discord.MessageEmbed()
+												.setColor(`#0099ff`)
+												.setTitle(`Totally legit dice`)
+												.addField(`You \"rolled\" a:`, Math.floor(Math.random() * 10), true)
+												.addField(`\"roll\" number:`, `${i + 1}/${multidice[1]}`);
+											message.channel.send(number10);
+										}
+									} else if (multidice[0] === `100`) {
+										for (i = 0; i < multidice[1]; i++) {
+											var number100 = new Discord.MessageEmbed()
+												.setColor(`#0099ff`)
+												.setTitle(`Totally legit dice`)
+												.addField(`You \"rolled\" a:`, Math.floor(Math.random() * 10) * 10, true)
+												.addField(`\"roll\" number:`, `${i + 1}/${multidice[1]}`);
+											message.channel.send(number100);
+										}
 									} else {
-										if (multidice[0] === `10`) {
-											for (i = 0; i < multidice[1]; i++) {
-												var number10 = new Discord.MessageEmbed()
-													.setColor(`#0099ff`)
-													.setTitle(`Totally legit dice`)
-													.addField(`You \"rolled\" a:`, Math.floor(Math.random() * 10), true)
-													.addField(`\"roll\" number:`, `${i + 1}/${multidice[1]}`);
-												message.channel.send(number10);
-											}
-										} else if (multidice[0] === `100`) {
-											for (i = 0; i < multidice[1]; i++) {
-												var number100 = new Discord.MessageEmbed()
-													.setColor(`#0099ff`)
-													.setTitle(`Totally legit dice`)
-													.addField(`You \"rolled\" a:`, Math.floor(Math.random() * 10) * 10, true)
-													.addField(`\"roll\" number:`, `${i + 1}/${multidice[1]}`);
-												message.channel.send(number100);
-											}
-										} else {
-											for (i = 0; i < multidice[1]; i++) {
-												var numberN = new Discord.MessageEmbed()
-													.setColor(`#0099ff`)
-													.setTitle(`Totally legit dice`)
-													.addField(`You \"rolled\" a:`, Math.ceil(Math.random() * Number(multidice[0])), true)
-													.addField(`\"roll\" number:`, `${i + 1}/${multidice[1]}`);
-												message.channel.send(numberN);
-											}
+										for (i = 0; i < multidice[1]; i++) {
+											var numberN = new Discord.MessageEmbed()
+												.setColor(`#0099ff`)
+												.setTitle(`Totally legit dice`)
+												.addField(`You \"rolled\" a:`, Math.ceil(Math.random() * Number(multidice[0])), true)
+												.addField(`\"roll\" number:`, `${i + 1}/${multidice[1]}`);
+											message.channel.send(numberN);
 										}
 									}
-								} else {
-									return;
 								}
-							} else if (message.content.toLowerCase().replace(`d`, ``) === `10`) {
-								var number10 = new Discord.MessageEmbed()
-									.setColor(`#0099ff`)
-									.setTitle(`Totally legit dice`)
-									.addField(`You \"rolled\" a:`,
-										Math.floor(Math.random() * 10), true);
-								message.channel.send(number10);
-							} else if (message.content.toLowerCase().replace(`d`, ``) === `100`) {
-								var number100 = new Discord.MessageEmbed()
-									.setColor(`#0099ff`)
-									.setTitle(`Totally legit dice`)
-									.addField(`You \"rolled\" a:`, Math.floor(Math.random() * 10) * 10, true);
-								message.channel.send(number100);
 							} else {
-								var numberN = new Discord.MessageEmbed()
-									.setColor(`#0099ff`)
-									.setTitle(`Totally legit dice`)
-									.addField(`You \"rolled\" a:`,
-										Math.ceil(Math.random() *
-											Number(message.content.toLowerCase().replace(`d`, ``))
-										),
-										true
-									);
-								message.channel.send(numberN);
+								return;
 							}
+						} else if (message.content.toLowerCase().replace(`${prefix}d`, ``) === `10`) {
+							var number10 = new Discord.MessageEmbed()
+								.setColor(`#0099ff`)
+								.setTitle(`Totally legit dice`)
+								.addField(`You \"rolled\" a:`,
+									Math.floor(Math.random() * 10), true);
+							message.channel.send(number10);
+						} else if (message.content.toLowerCase().replace(`${prefix}d`, ``) === `100`) {
+							var number100 = new Discord.MessageEmbed()
+								.setColor(`#0099ff`)
+								.setTitle(`Totally legit dice`)
+								.addField(`You \"rolled\" a:`, Math.floor(Math.random() * 10) * 10, true);
+							message.channel.send(number100);
+						} else {
+							var numberN = new Discord.MessageEmbed()
+								.setColor(`#0099ff`)
+								.setTitle(`Totally legit dice`)
+								.addField(`You \"rolled\" a:`,
+									Math.ceil(Math.random() *
+										Number(message.content.toLowerCase().replace(`${prefix}d`, ``))
+									),
+									true
+								);
+							message.channel.send(numberN);
 						}
 					}
-				});
+				}
 			}
 		}
 		//userinfo
@@ -742,6 +740,8 @@
 						message.channel.send(url)
 					})
 				}
+
+				dice(message, GLaDOSPrefix);
 			})
 		}
 		//forwarding
@@ -770,10 +770,6 @@
 				}
 			}
 		});
-	}
-	//dice roll
-	{
-		dice(glados);
 	}
 	//maze thing
 	{
@@ -1239,7 +1235,9 @@
 	}
 	//dice roll
 	{
-		dice(sini);
+		sini.on(`message`, (message) => {
+			dice(message, ``);
+		})
 	}
 	//joinVC
 	{
