750326eea33ec6c4844d94985d3da3d1ddf34955
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2021-05-30 16:00:00 +0200

    Added documentation to thte inspirobot modules

--- GLaDOS/inspiroBot.js
+++ GLaDOS/inspiroBot.js
@@ -1,4 +1,7 @@
 const req = require('node-fetch')
+/**
+ * @returns {Promise<string>} A promise to a random inspirobot URL.
+ */
 const getURL = () => {
     return req(`https://inspirobot.me/api?generate=true`)
     .then((res) => {
