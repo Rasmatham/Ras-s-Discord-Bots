aa2795adc2e06e6f406836d556bcdf974113b1ef
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2021-06-01 15:28:38 +0200

    I couldn't bother with it, so I'm just saving them as .log files instead

--- custom_modules/coinflip.js
+++ custom_modules/coinflip.js
@@ -9,15 +9,15 @@ var setup = (message, prefix) => {
     if (message.content.toLowerCase().startsWith(`${prefix}cfsetup`)) {
         message.channel
             .send(`Creating save for ${message.author}`)
-            .then(() => mkdir(`userinfo/`, (err) => { }))
+            .then(() => mkdir(`./${message.client.user.username}/userinfo/`, (err) => { }))
             .then(() =>
                 mkdir(`./${message.client.user.username}/userinfo/${message.author.id}`, (err) => { })
             )
             .then(() => {
                 mkdir(`./${message.client.user.username}/userinfo/${message.author.id}/coinflip/`, (err) => { })
             })
-            .then(() => { writeFile(`./${message.client.user.username}/userinfo/${message.author.id}/coinflip/wins.txt`, `0`, (err) => { }) })
-            .then(() => { writeFile(`./${message.client.user.username}/userinfo/${message.author.id}/coinflip/losses.txt`, `0`, (err) => { }) });
+            .then(() => { writeFile(`./${message.client.user.username}/userinfo/${message.author.id}/coinflip/wins.log`, `0`, (err) => { }) })
+            .then(() => { writeFile(`./${message.client.user.username}/userinfo/${message.author.id}/coinflip/losses.log`, `0`, (err) => { }) });
         message.channel.send(`Setup complete\nWarning: Using this command agin will reset your score`);
     }
 }
@@ -29,8 +29,8 @@ var setup = (message, prefix) => {
 var flip = (message, prefix) => {
     if (message.content.toLowerCase().startsWith(`${prefix}coinflip`)) {
         var coinflippath = `./${message.client.user.username}/userinfo/${message.author.id}/coinflip/`;
-        var coinfilew = `./${message.client.user.username}/userinfo/${message.author.id}/coinflip/wins.txt`;
-        var coinfilel = `./${message.client.user.username}/userinfo/${message.author.id}/coinflip/losses.txt`;
+        var coinfilew = `./${message.client.user.username}/userinfo/${message.author.id}/coinflip/wins.log`;
+        var coinfilel = `./${message.client.user.username}/userinfo/${message.author.id}/coinflip/losses.log`;
         if (existsSync(coinflippath)) {
             if (message.content.toLowerCase().startsWith(`${prefix}coinflip heads`)) {
                 if (Math.random() * 10 < 5 === true) {
