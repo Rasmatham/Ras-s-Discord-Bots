96758bcbd204671f5fde29974c69d3d9db3228c2
RasMatHam <61523831+Rasmatham@users.noreply.github.com>
2022-03-27 00:29:48 +0100

    If this works, it might be the last thing I need to do

--- piSetup.sh
+++ piSetup.sh
@@ -1,18 +1,39 @@
+# make sure everything is up to date bfore starting
 sudo apt-get update
 sudo apt-get -y dist-upgrade
+
+#install necessary things
 sudo apt-get install -y git
 curl -sL https://deb.nodesource.com/setup_current.x | sudo -E bash -
 sudo apt-get install -y nodejs
-cd
-rm -rf Ras-s-Discord-Bots
-git clone https://github.com/Rasmatham/Ras-s-Discord-Bots.git
-cd
-cd Ras-s-Discord-Bots
 sudo npm i -g npm@latest
 sudo npm i -g typescript
+
+#make sure there will be no conflicts
+cd
+rm -rf Ras-s-Discord-Bots
+
+#clone bot code and go to directory
+git clone https://github.com/Rasmatham/Ras-s-Discord-Bots.git
+cd Ras-s-Discord-Bots
+
+#remove potentially confusing files
+rm piSetup.sh Bots.bat ActivateCommands.bat
+
+#install missing modules
 npm i
+
+#compile
 tsc
+
+#get bot tokens
 git clone https://Rasmatham:$1@github.com/Rasmatham/Discord-Bot-Tokens.git
 mv Discord-Bot-Tokens/.env .
 rm -rf Discord-Bot-Tokens
-node AllTheBots
+
+#set autorun script
+sed -i "s/ABC/$1/" Bots.sh
+mv temp.sh /etc/init.d/Bots.sh
+
+#run bots
+node AllTheBots.js
---
+++ temp.sh
@@ -0,0 +1,22 @@
+#make sure there will be no conflicts
+cd
+rm -rf Ras-s-Discord-Bots
+
+#clone bot code and go to directory
+git clone https://github.com/Rasmatham/Ras-s-Discord-Bots.git
+cd Ras-s-Discord-Bots
+
+#remove potentially confusing files
+rm piSetup.sh Bots.bat ActivateCommands.bat temp.sh
+
+#install missing modules
+npm i
+
+#compile
+tsc
+
+#get bot tokens
+git clone https://Rasmatham:TOKEN@github.com/Rasmatham/Discord-Bot-Tokens.git
+mv Discord-Bot-Tokens/.env .
+rm -rf Discord-Bot-Tokens
+node AllTheBots.js
