52dcbbffa3ecae083413c5f9ae379a365d7e7d45
RasMatHam <rasmatham@gmail.com>
2022-07-22 15:37:40 +0200

    Fixed GeneralUse

--- custom_modules/generalUse.ts
+++ custom_modules/generalUse.ts
@@ -1,5 +1,5 @@
 //#region imports
-import {Message, TextChannel, WebhookClient, Client, MessageOptions, Webhook, BufferResolvable, User, IntentsBitField, NewsChannel, APIEmbedField, ClientUser, InteractionReplyOptions, CommandInteraction, CommandInteractionOption, EmbedBuilder} from "discord.js";
+import {Message, TextChannel, WebhookClient, Client, MessageOptions, Webhook, BufferResolvable, User, IntentsBitField, NewsChannel, EmbedField, ClientUser, InteractionReplyOptions, CommandInteraction, CommandInteractionOption, EmbedBuilder} from "discord.js";
 import * as os from "os";
 //#endregion
 
@@ -32,8 +32,8 @@ export const checkFor = (
 		str: string,
 		inline?:boolean
 	}[]
-):APIEmbedField[] => {
-	const out:APIEmbedField[] = [];
+):EmbedField[] => {
+	const out:EmbedField[] = [];
 	inObjs.forEach((inObj) => {
 		if (inObj.arr.length > 0) {
 			if (inObj.inline == null) {
